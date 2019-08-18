31ff48d1b2bac390f92ec5d13b468f0d0fc238d9
clab02 <belabrockmann@gmail.com>
2019-08-18 07:46:07 +0200

    Added title and axis labels

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ document.addEventListener("DOMContentLoaded", () => {
         const height = window.innerHeight - 50;
         const width = window.innerWidth - 50;
         const margin = {
-            top: 20,
+            top: 70,
             right: 20,
-            bottom: 20,
-            left: 20
+            bottom: 45,
+            left: 65
         };
         const innerHeight = height - margin.top - margin.bottom;
         const innerWidth = width - margin.right - margin.left;
@@ -51,7 +51,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const g = svg.append("g")
             .attr("height", innerHeight)
             .attr("width", innerWidth)
-            .attr("transform", `translate(${margin.left}, ${margin.right})`);
+            .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
         const areaGenerator = d3.area()
             .x(d => xScale(d[0]))
@@ -69,6 +69,22 @@ document.addEventListener("DOMContentLoaded", () => {
 
         g.append("g")
             .call(yAxis);
+
+        g.append("text")
+            .attr("id", "title")
+            .text(dataset.description.title)
+            .attr("y", -10);
+        g.append("text")
+            .attr("class", "label")
+            .text("Years")
+            .attr("x", innerWidth / 2)
+            .attr("y", innerHeight + 40);
+        g.append("text")
+            .attr("class", "label")
+            .text("Precipitation in inches")
+            .attr("x", -innerHeight / 2)
+            .attr("transform", "rotate(-90)")
+            .attr("y", -40)
     }
     fetch("https://www.ncdc.noaa.gov/cag/national/time-series/110-pcp-ytd-12-1895-2016.json?base_prd=true&begbaseyear=1901&endbaseyear=2000")
         .then(response => response.json())
